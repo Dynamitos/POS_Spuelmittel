f0a5b224c90859e6f994f5fa229f982240982f37
seichc13@htl-kaindorf.ac.at <seichc13@htl-kaindorf.ac.at>
2018-02-09 12:31:02 +0100

    implemented simpleResponse feature

--- web/demoAjax/demo01.js
+++ web/demoAjax/demo01.js
@@ -2,6 +2,17 @@
 var xmlHttp = initAjax();
 
 function startRequest() {
-    
+    xmlHttp.onreadystatechange = handleStateChange;
+    xmlHttp.open("GET", "../res/simpleResponse.txt", true);
+    xmlHttp.send(null);
+}
+
+//Callback function
+function handleStateChange() {
+    if(xmlHttp.readyState === 4) {
+        if(xmlHttp.status === 200) {
+            alert("Antwort vom Server: " + xmlHttp.responseText);
+        }
+    }
 }
  
